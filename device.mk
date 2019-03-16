e5c29a855a9720fd8d3d66327835238ceb51473a
Subhajeet Muhuri <kenny3fcb@gmail.com>
2019-03-16 16:21:59 +0530

    X00P: libperfmgr: Import powerhal from crosshatch

--- device.mk
+++ device.mk
@@ -295,13 +295,12 @@ PRODUCT_PACKAGES += \
     charger_res_images \
     product_charger_res_images
 
-# Power
+# Power-libperfmgr
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power@1.3-service.crosshatch-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # QMI
 PRODUCT_PACKAGES += \
