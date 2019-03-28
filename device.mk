4e71c2df2abf2ecafd0c1df70efc2bab54834c26
kondors1995 <normandija1945@gmail.com>
2019-03-28 05:04:36 +0000

    X00P: libperfmgr: Rename crosshatch to X00P
    Lets add power config to
    This version has striped down cpu boost

--- device.mk
+++ device.mk
@@ -297,7 +297,7 @@ PRODUCT_PACKAGES += \
 
 # Power-libperfmgr
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.crosshatch-libperfmgr
+    android.hardware.power@1.3-service.X00P-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
