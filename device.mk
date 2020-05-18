eadbd4ce07bb8f643ad2a5f2783a02940cef2872
iamsaalim <saalimquadri1@gmail.com>
2020-05-19 00:03:36 +0530

    X00P: drop telephony boot jar * already built from source
    Signed-off-by: iamsaalim <saalimquadri1@gmail.com>
    Change-Id: If2180c910b2280a4094beec90696d96d7869af8d

--- device.mk
+++ device.mk
@@ -360,9 +360,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
