74ecf96103f0a6c661aafbe11d2453c1ec7cdcae
Jyotiraditya <jyotiraditya@aospa.co>
2020-08-19 17:26:05 +0000

    X00P: Make building of power hal commonized
    Change-Id: I7f651efc22cece97f4d52756ecd6d844ba9c767e

--- device.mk
+++ device.mk
@@ -290,10 +290,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
-
+# Powerhint
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
